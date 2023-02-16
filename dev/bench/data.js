9c084b2901169e59952211f468730c532aa5a62e
github-action-benchmark <github@users.noreply.github.com>
2023-02-16 22:38:38 +0300

    add BFS (benchmarkdotnet) benchmark result for de39e5b0e43966bdc8fb5ee66f711aa407c0daa5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675666694990,
+  "lastUpdate": 1676576318321,
   "repoUrl": "https://github.com/kirillgarbar/GraphBLAS-sharp",
   "entries": {
     "BFS": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 245016170.5,
             "unit": "ns",
             "range": "± 14651413.3680102"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "committer": {
+            "email": "st087492@student.spbu.ru",
+            "name": "kirillgarbar",
+            "username": "kirillgarbar"
+          },
+          "distinct": true,
+          "id": "de39e5b0e43966bdc8fb5ee66f711aa407c0daa5",
+          "message": "New token, workflow dispatch",
+          "timestamp": "2023-02-16T15:48:26+03:00",
+          "tree_id": "e7b03ad5802a8050db323e851ca67c9c67502460",
+          "url": "https://github.com/kirillgarbar/GraphBLAS-sharp/commit/de39e5b0e43966bdc8fb5ee66f711aa407c0daa5"
+        },
+        "date": 1676576317604,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "wing.mtx",
+            "value": 251538767.3,
+            "unit": "ns",
+            "range": "± 17621404.181083135"
           }
         ]
       }
